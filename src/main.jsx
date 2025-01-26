3babe020a2c42f553b501c0fa24ecc55d50f5ca8
rimu-7 <rrimu99@gmail.com>
2025-01-26 15:57:21 +0800

    react theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './Routes/Routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className="container mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
+
