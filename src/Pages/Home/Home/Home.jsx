8250c960b276a2f87776e82a3bbb7ab1a79fe16a
rimu-7 <rrimu99@gmail.com>
2025-01-26 16:10:12 +0800

    home

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div className='h-screen bg-[#212121] text-center items-center flex justify-center text-2xl text-white'>Home</div>
-  )
+    <div className="h-screen bg-[#212121] text-center items-center flex justify-center text-2xl text-white">
+      Home
+    </div>
+  );
 }
 
-export default Home
+export default Home;
